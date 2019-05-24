08ddc7bae7ce67bc0a861d049d94d41848b4c375
ElHussein Ali <marwa.elhusseinali1@vodafone.com>
2019-05-24 16:22:27 +0200

    vedio 45 udemy

--- src/App.js
+++ src/App.js
@@ -7,11 +7,22 @@ class App extends Component {
       {name: "max", age:30}
     ]
   }
+  switchNameHandler = (newName)=>{
+    this.setState({
+      persons:[
+        {name: newName, age:27}
+      ]
+    })
+  }
   render() {
     return (
       <div className="App">
        <h1> hi . i'm react app </h1>
-       <Person name={this.state.persons[0].name}  age={this.state.persons[0].age}>my Hobbies: racing</Person>
+       <button onClick={()=>this.switchNameHandler('maximillian')}>Switch Name</button>
+       <Person 
+       name={this.state.persons[0].name}  
+       age={this.state.persons[0].age}
+       click={this.switchNameHandler.bind(this,'marwa')}>my Hobbies: racing</Person>
       </div>
     );
     // return React.createElement('div',{className:'App'},React.createElement('h1',null,'hi . i\'m react app '));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ const person = (props) => {
     
     return <div>
         <p>i'm {props.name } my age is {props.age}</p>
-        <p>{props.children}</p>
+        <p onClick={props.click}>{props.children}</p>
         </div>
 }
 export default person;
